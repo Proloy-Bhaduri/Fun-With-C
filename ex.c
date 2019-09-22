04b09a4003c3b060534577c7e4dfe76997dc15a3
Proloy Bhaduri <42575314+Proloy-Bhaduri@users.noreply.github.com>
2019-09-23 01:24:35 +0530

    Update ex.c

--- ex.c
+++ ex.c
@@ -13,14 +13,12 @@
       scanf("%d",&p);
       printf("The series of e^x up to %d terms is :\n",p);
       for(i=0;i<=p;i++)
-      {  
-         printf(" (x^%d/%d!) + ",i,i);
-      }
-     printf("\nEnter the value of x: \n");
-     scanf("%d",&q);
-     for(i=0;i<=p;i++)
-            s = s + ((float) pow(q,i)/(float) fact(i));
-    printf("The value of e^%d using exp. function up to %d terms is: %f ",q,p,s);
+            printf(" (x^%d/%d!) + ",i,i);
+      printf("\nEnter the value of x: \n");
+      scanf("%d",&q);
+      for(i=0;i<=p;i++)
+             s = s + ((float) pow(q,i)/(float) fact(i));
+       printf("The value of e^%d using exp. function up to %d terms is: %f ",q,p,s);
     }//end of main 
   	int fact(int a)
   		{
