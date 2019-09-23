dbdf4e6ed2789ec126582d51d8dda1e72837cb88
Proloy Bhaduri <42575314+Proloy-Bhaduri@users.noreply.github.com>
2019-09-23 17:57:09 +0530

    Update ex.c

--- ex.c
+++ ex.c
@@ -1,6 +1,6 @@
 
 /* C Program to find the value of e^x using exponential function , and x is taken from the user */ 
-
+<s>
 #include<stdio.h>
 #include<math.h>
  int fact(int);
@@ -26,4 +26,32 @@
   			for(j=1;j<=a;j++) fact = fact * j;
   			if(a==0) return 1;
   			else return fact;
-  		}
+  		}</s>
+#include<stdio.h>
+#include<math.h>
+ float fact(float);
+
+  int main()
+    {
+      int i,p,q,k;
+      float s=0;
+      printf("Enter the number of terms you want to see in the exponential function\n");
+      scanf("%d",&p);
+      printf("The series of e^x up to %d terms is :\n",p);
+      for(i=0;i<=p;i++)
+            printf(" (x^%d/%d!) + ",i,i);
+      printf("\nEnter the value of x: \n");
+      scanf("%d",&q);
+      for(i=0;i<=p;i++)
+             p = 
+             s = s + (float) {pow(q,i)/fact(i)};
+       printf("The value of e^%d using exp. function up to %d terms is: %f ",q,p,s);
+    }//end of main 
+    float fact(float a)
+      {
+        int j; 
+        float fact=1.0f;
+        for(j=1;j<=a;j++) fact = fact * j;
+        if(a==0) return 1.0f;
+        else return fact;
+      }
