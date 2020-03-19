3ca26478e21d31d48c40782c25c6de82e05d6b6c
Proloy Bhaduri <42575314+Proloy-Bhaduri@users.noreply.github.com>
2020-03-19 13:18:23 +0530

    Create prime_number.c

---
+++ prime_number.c
@@ -0,0 +1,24 @@
+//gcc 5.4.0
+
+#include  <stdio.h>
+
+int main(void)
+{
+    int num,i=2,flag=0;
+    printf("Enter a number \n");
+    scanf("%d",&num);
+   for(; i<num;i++)
+    {
+         if(num%i == 0)
+         {
+            flag = 1;
+            break;
+         }
+      else continue;
+    }
+    if(flag == 0)
+        printf("\nPrime");
+    else
+        printf("\n Not Prime");
+    return 0;
+}
