92e2de300f9ed1f6f3a332125481285d59c0ce71
Proloy Bhaduri <42575314+Proloy-Bhaduri@users.noreply.github.com>
2020-03-19 15:18:30 +0530

    Create factors_of(number).c
    Program to calculate  factors of a number

---
+++ factors_of(number).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int factors_of(int);
+int main(void)
+{
+    int num;
+    printf("Enter a number \n");
+    scanf("%d",&num);
+    printf("Factors of %d are:\n",num);
+    factors_of(num);
+    return 0;
+}
+int factors_of(int a)
+     {
+       int i=1;
+       for(; i<=a;i++)
+        {
+         if(a%i == 0)
+           {
+            printf("%d ",i);
+           }
+        else continue;
+       }
+    }
